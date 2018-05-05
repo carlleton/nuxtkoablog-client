be70d42a06922d8ca3624ba0ade3240b0ae56d24
carlleton <carlleton@hotmail.com>
2018-05-05 22:45:44 +0800

    elemntui

---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-form ref="form" :model="user" label-width="80px">
+      <el-form-item label="用户名：">
+        <el-input v-model="user.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码：">
+        <el-input v-model="user.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
