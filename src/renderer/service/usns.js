b12e7ef3db266c1b5ee3d341c7de3ccb0b7e5933
carlleton <carlleton@hotmail.com>
2018-06-21 12:43:30 +0800

    更新tagid时判断是否本地已经更改过，避免服务端处理出错

--- src/renderer/service/usns.js
+++ src/renderer/service/usns.js
@@ -3,6 +3,20 @@ import base from './base'
 let {tableids} = require('../config/state')
 let usns = _.extend(_.extend({}, base), {
   name: 'usns',
+  onebytagid (tag, id) {
+    let params = {}
+    params.tag = tag
+    params.tagid = id
+    return new Promise((resolve, reject) => {
+      this.Doc.findOne(params).exec((err, doc) => {
+        if (!err) {
+          resolve(doc)
+        } else {
+          reject(err)
+        }
+      })
+    })
+  },
   async syncadd (name, id) {
     let tag = tableids[name]
     return this.add({
@@ -26,6 +40,10 @@ let usns = _.extend(_.extend({}, base), {
   },
   async syncupdate (name, id) {
     let tag = tableids[name]
+    let usn = await this.onebytagid(tag, id)
+    if (usn && usn.deal === 0) {
+      return
+    }
     return this.update({
       tag,
       tagid: id
